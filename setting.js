41622525fd5151f69668657100e188ded5a88eef
malzzganz-cyber <retio1766@gmail.com>
2026-05-03 20:24:17 +0700

    Add setting.js

---
+++ setting.js
@@ -0,0 +1,5 @@
+// Ganti dengan API key kamu dari VPedia
+global.apikey = process.env.VPEDIA_API_KEY || 'KhafaTopUp_yjmm32s92qz3jn0d';
+
+// Fee tambahan per transaksi (dalam rupiah), set 0 kalau tidak mau
+global.feenya = parseInt(process.env.FEE || '300');
